84581a6d629a8232e184610b4b2ba41153851157
Maroni19 <maroni101010@gmail.com>
2018-02-22 16:01:11 +0200

    prod

--- js/build.js
+++ js/build.js
@@ -1,10 +1,1 @@
-$(function () {
-    console.log('ham');
-    $('._hamburger').click(function () {
-        $('._nav').slideToggle();
-    });
-});
-console.log('module 1 have done');
-console.log('module 2 have done');
-console.log('module 3 have done');
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImhhbWJ1cmdlci5qcyIsIm1vZHVsZTEuanMiLCJtb2R1bGUyLmpzIiwibW9kdWxlMy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUNMQTtBQ0FBO0FDQUEiLCJmaWxlIjoiYnVpbGQuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGZ1bmN0aW9uICgpIHtcclxuICAgIGNvbnNvbGUubG9nKCdoYW0nKTtcclxuICAgICQoJy5faGFtYnVyZ2VyJykuY2xpY2soZnVuY3Rpb24gKCkge1xyXG4gICAgICAgICQoJy5fbmF2Jykuc2xpZGVUb2dnbGUoKTtcclxuICAgIH0pO1xyXG59KTsiLCJjb25zb2xlLmxvZygnbW9kdWxlIDEgaGF2ZSBkb25lJyk7IiwiY29uc29sZS5sb2coJ21vZHVsZSAyIGhhdmUgZG9uZScpOyIsImNvbnNvbGUubG9nKCdtb2R1bGUgMyBoYXZlIGRvbmUnKTsiXX0=
+$(function(){console.log("ham"),$("._hamburger").click(function(){$("._nav").slideToggle()})}),console.log("module 1 have done"),console.log("module 2 have done"),console.log("module 3 have done");
